9ac7bc95912395f0bf0eae396dfeb1465f9cf892
c_lumapay <clumapay.info@gmail.com>
2021-05-05 16:32:55 +0100

    Mastering classes by working with calculator

--- main.js
+++ main.js
@@ -1,68 +1,40 @@
 class Calculator {
-    constructor(displayElement1, displayElement2, displayOperators) {
-        this.displayElement1 = displayElement1;
-        this.displayElement2 = displayElement2;
+    constructor(displayElementX, displayElementY, displayOperators) {
+        this.displayElementX = displayElementX;
+        this.displayElementY = displayElementY;
         this.displayOperators = displayOperators;
         this.clear();
     }
 
+    // clear and delete elements 
     clear() {
-        this.display1 = '';
-        this.display2 = '';
-        this.allOperators = '';
+        this.displayX = '';
+        this.displayY = '';
+        this.operators = '';
         this.dot = false;
-        this.result = null;
+        this.result = '';
+    }
+    delete() {}
+    clearEntity() {}
+
+    // Buttons 
+    updateDsiplay(operation) {
+        if(operation === '.' && !this.displayY) this.dot = true;
+        if(operation === '.' && this.displayY.includes('.')) return;
+        this.displayY += operation;
+    }
+    appendUpdates() {
+        this.displayElementY.innerText = this.displayY;
     }
 
-    appendNumber(number) {
-        if (number === '.' && !this.display2.includes('.')) this.dot = true;
-        else if (number === '.' && this.display2.includes('.')) return;
-        this.display2 += number;
-    }
-
-    updateOutPut() {
-        this.displayElement2.innerText = this.display2;
-    }
-
-    updateOperator(operator) {
-        this.dot = false;
-        if(this.display1 && this.display2 && this.allOperators) {
-            this.compute();
-            this.operation();
-        } else {
-            this.displayOperators.innerText = this.allOperators;
-            this.display1 += this.display2;
-            this.displayElement1.innerText = this.display1;
-            this.display2 = '';
-            this.displayElement2.innerText = '';
-        }
-        this.allOperators = operator;
-        this.displayOperators.innerText = this.allOperators;
-        this.display1 += this.display2;
-        this.displayElement1.innerText = this.display1
-        this.display2 = '';
-        this.displayElement2.innerText = '';
-    }
-
-    compute(operator) {
-        if (operator === '*') {
-            const tempresult = parseFloat(this.display1) * parseFloat(this.display2);
-            this.result = tempresult;
-        }
-    }
-
-    operation() {
-        this.display2 = ''
-        this.displayElement1.innerText = '';
-        this.displayElement2.innerText = this.result;
-        this.display1 = this.result;
-        this.allOperators = '';
-        this.displayOperators.innerText = this.allOperators;
-    }
+    // operators
+    updateOperator() {}
+    compute() {}
+    computeOperation() {} 
 }
 
-const displayElement1 = document.querySelector('.small-output');
-const displayElement2 = document.querySelector('.current-output');
+const displayElementX = document.querySelector('.small-output');
+const displayElementY = document.querySelector('.current-output');
 const displayOperators = document.querySelector('.operators-output');
 const allNumbers = document.querySelectorAll('.number')
 const allOperators = document.querySelectorAll('.data_operators');
@@ -71,20 +43,22 @@ const clear = document.querySelector('.clear')
 const ce = document.querySelector('.ce')
 const del = document.querySelector('.del_image');
 
-const calculator = new Calculator(displayElement1, displayElement2, displayOperators);
+const calculator = new Calculator(displayElementX, displayElementY, displayOperators);
 
 //Buttons: code here
 
 allNumbers.forEach(number => {
     number.addEventListener('click', e => {
-        calculator.appendNumber(e.target.innerText);
-        calculator.updateOutPut();
+        calculator.updateDsiplay(e.target.innerText);
+        calculator.appendUpdates();
     })
 })
 
-allOperators.forEach(operator => {
-    operator.addEventListener('click', e => {
-        calculator.updateOperator(e.target.innerText)
-        calculator.compute(e.target.innerText);
-    })
+
+
+
+
+
+clear.addEventListener('click', () => {
+    calculator.clear();
 })
